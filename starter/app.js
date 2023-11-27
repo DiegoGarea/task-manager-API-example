c53ccf6972e543f936a817ca35a14e68475211ca
Diego G <d.g.g.87@hotmail.com>
2023-11-27 12:06:20 -0600

    setup with postman

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/task');
+
+//middleware
+
+app.use(express.json());
+
+// routes
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
