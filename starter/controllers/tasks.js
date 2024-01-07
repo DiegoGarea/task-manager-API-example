59ae47a3a096d1f93349b411e4eb8c82060a61a8
Diego G <d.g.g.87@hotmail.com>
2024-01-07 06:36:03 -0600

    added middleware for any route that doesnt exist

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -4,6 +4,8 @@ const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
     res.status(200).json({tasks});
+    // res.status(200).json({tasks,amount: tasks.length});
+    // res.status(200).json({status: 'success', data: tasks});
   } catch (error) {
     res.status(500).json({msg: error});
   }
@@ -60,10 +62,28 @@ const updateTask = async (req, res) => {
   }
 };
 
+// const editTask = async (req, res) => {
+//   try {
+//     const {id: taskID} = req.params;
+//     const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+//       new: true,
+//       runValidators: true,
+//       overweite: true,
+//     });
+//     if (!task) {
+//       return res.status(404).json({msg: `No task with id: ${taskID}`});
+//     }
+//     res.status(200).json({task});
+//   } catch (error) {
+//     res.status(500).json({msg: error});
+//   }
+// };
+
 module.exports = {
   getAllTasks,
   createTask,
   getTask,
   updateTask,
   deleteTask,
+  // editTask,
 };
